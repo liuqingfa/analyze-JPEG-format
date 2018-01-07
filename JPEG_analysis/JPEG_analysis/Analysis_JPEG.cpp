cb6b00d2c81702d26e842a899421e3eb8dc75b61
rina2 <lhwlina1130@naver.com>
2018-01-07 20:28:01 +0900

    commit

--- JPEG_analysis/JPEG_analysis/Analysis_JPEG.cpp
+++ JPEG_analysis/JPEG_analysis/Analysis_JPEG.cpp
@@ -9,8 +9,20 @@
 
 
 int main() {
+    WORD isSOF;
+    HEADER header;
+    HEADER_ADDTIONAL header_additional;
+    SOF_MARK sof_mark;
+    DQT_MARK dqt_mark[4];
+    DHT_MARK dht_mark;
+    SOS_MARK sos_mark;
+    DRI_MARK dri_mark;
+    dht_mark.numOfhuff = 0;
 
     char tmp[5];
+
+
+    /////file open////
     FILE* jpg;
     fopen_s(&jpg, "test.jpg", "rb");
     if (jpg == NULL) {
@@ -18,46 +30,143 @@ int main() {
         return -1;
     }
 
-    
-    HEADER header;
-    
-    fread(&header, sizeof(HEADER), 1, jpg);
-    if (header.soi != SOI) {
-        printf("different SOI");
-        return -1;
-    }
-    if (header.app != APP0) {
-        printf("different APP0");
-        return -1;
-    }
+    int check_error = TRUE;
+    WORD tmpword;
+   // while (1) {
+        fread(&tmpword, sizeof(WORD), 1, jpg);
+        switch (tmpword) {
+        case SOI: check_error = readHeader(jpg, &header, &header_additional, tmpword); break;
+       // case DQT: readDQT(jpg, dqt_mark, tmpword); break;
+        }
+        printf("tmpword : %x", tmpword);
+        //if (check_error != TRUE) break;
+        //if (tmpword == SOI) break;
+    //}
+    if (check_error != TRUE) return -1;
 
-    header.data_len = to_big(header.data_len);
+    fread(&tmpword, sizeof(WORD), 1, jpg);
+    printf("next : %x\n", tmpword);
+    /*
     
-    BYTE* additional_data = (BYTE*)malloc(sizeof(BYTE)*(header.data_len - 2));
-    fread(additional_data, sizeof(BYTE)*(header.data_len-2), 1, jpg);
-    
-    //To Do: check additional data  --> Read Commnet, too ( FFEE )_
 
    
-
     ////////DQT : Define Quantization Tables/////////////
     WORD isDQT;
-    BYTE* DQT_table[4];
     
-
     while (fread(&isDQT, sizeof(WORD), 1, jpg)) {
         if (isDQT != DQT) break;
         WORD table_size;
         BYTE idx;
         fread(&table_size, sizeof(WORD), 1, jpg);
         fread(&idx, sizeof(BYTE), 1, jpg);
-        table_size = to_big(table_size);
-        DQT_table[idx] = (BYTE*)malloc(sizeof(BYTE)*(table_size - 3));
-        fread(DQT_table[idx], sizeof(BYTE)*(table_size - 3), 1, jpg);
+        dqt_mark[idx].data_len = table_size = to_big(table_size);
+        dqt_mark[idx].DQT_tables = (BYTE*)malloc(sizeof(BYTE)*(table_size - 3));
+        fread(dqt_mark[idx].DQT_tables , sizeof(BYTE)*(table_size - 3), 1, jpg);
         idx++;
     };
 
    fseek(jpg, -2L, SEEK_CUR);
-    
+
+   //////start of  frame////////
+   fread(&isSOF, sizeof(WORD), 1, jpg);
+   if (isSOF != SOF) {
+       printf("%x\n", isSOF);
+       printf("invalid format");
+       return -1;
+   }
+   fread(&(sof_mark.SOF_len), sizeof(BYTE) * 2,1,jpg);
+   sof_mark.SOF_len = to_big(sof_mark.SOF_len);
+   sof_mark.SOF_data = (BYTE*)malloc(sizeof(BYTE)*(sof_mark.SOF_len - 2));
+   fread(sof_mark.SOF_data, sizeof(BYTE)*(sof_mark.SOF_len - 2), 1, jpg);
+
+   
+   ////////DHT marker : huffman code table//////
+   WORD isDHT;
+   int idx = 0;
+   while (1) {
+       fread(&isDHT, sizeof(WORD), 1, jpg);
+       if (isDHT != DHT) break;
+       
+       WORD hf_len;
+       fread(&hf_len, sizeof(WORD), 1, jpg);
+       hf_len = to_big(hf_len);
+       dht_mark.huf_tables[idx].huff_len = hf_len-2;
+       dht_mark.huf_tables[idx].huff_table = (BYTE*)malloc(sizeof(BYTE)*(hf_len - 2));
+       fread(dht_mark.huf_tables[idx].huff_table, sizeof(BYTE)*(hf_len - 2), 1, jpg);
+       idx++;   
+   }
+
+   dht_mark.numOfhuff = idx;
+   fseek(jpg, -2L, SEEK_CUR);
+   
+
+   /////SOS : Start of Scan //////////
+   WORD isSOS;
+   fread(&isSOS, sizeof(WORD), 1, jpg);
+
+   if (isSOS != SOS) {
+       printf("invalid \n");
+       return -1;
+   }
+
+   fread(&sos_mark, sizeof(SOS_MARK), 1, jpg);
+
+    //// DRI : Define Restart interval/////
+   fread(&dri_mark, sizeof(DRI_MARK), 1, jpg);
+   if (dri_mark.DRI_mark != DRI) {
+       printf("invalid\n");
+       return -1;
+   }
+
+   
+ 
+*/
+   // if(header_additional.Thumbnail != NULL) free(header_additional.Thumbnail);
 }
 
+
+
+
+
+
+
+int readHeader(FILE* jpg, HEADER* header ,HEADER_ADDTIONAL* header_additional, WORD read) {
+    fread(&header, sizeof(HEADER)-2, 1, jpg);
+    header->soi_mark = read;
+    if (header->soi_mark != SOI || header->app != APP0) {
+        printf("invalid Header\n");
+        return FALSE;
+    }
+    header->data_len = to_big(header->data_len);
+    header_additional->Thumbnail = (BYTE*)malloc(sizeof(BYTE)*(header_additional->Xthumbnail * header_additional->Ythumbnail));
+    fread(header_additional, sizeof(BYTE)*(header->data_len - 2), 1, jpg);
+    char tmp[5];
+    memcpy(tmp, &(header_additional->identifier), 4);
+    tmp[4] = 0x00;
+    if (strcmp(tmp, "JFIF") != 0) {
+        printf("NOT JFIF\n");
+        return FALSE;
+    }
+    printf("tmp  : %s", tmp);
+}
+void readDQT(FILE* jpg, DQT_MARK* dqt_mark, WORD read) {
+    ////////DQT : Define Quantization Tables/////////////
+    
+    dqt_mark->DQT_mark = read;
+    while (1) {
+        if (dqt_mark->DQT_mark != DQT) break;
+        WORD table_size;
+        BYTE idx;
+        fread(&table_size, sizeof(WORD), 1, jpg);
+        fread(&idx, sizeof(BYTE), 1, jpg);
+        dqt_mark[idx].data_len = table_size = to_big(table_size);
+        dqt_mark[idx].DQT_tables = (BYTE*)malloc(sizeof(BYTE)*(table_size - 3));
+        fread(dqt_mark[idx].DQT_tables, sizeof(BYTE)*(table_size - 3), 1, jpg);
+        idx++;
+
+        fread(&(dqt_mark->DQT_mark), sizeof(WORD), 1, jpg);
+        printf("dqt_mark : %x\n", dqt_mark->DQT_mark);
+    };
+
+    fseek(jpg, -2L, SEEK_CUR);
+}
--- JPEG_analysis/JPEG_analysis/jpegdec.h
+++ JPEG_analysis/JPEG_analysis/jpegdec.h
@@ -2,11 +2,19 @@
 #define _JPEGDEC_H__
 
 #pragma pack(1)
+
+#define TRUE 1
+#define FALSE 0;
+
 typedef struct {
-    WORD soi;
+    WORD soi_mark;
     WORD app;
     WORD data_len;
-    /*
+   
+}HEADER;
+
+typedef struct {
+
     DWORD identifier;
     BYTE null;
     BYTE major_v;
@@ -16,14 +24,55 @@ typedef struct {
     WORD Y_density;
     BYTE Xthumbnail;
     BYTE Ythumbnail;
-    */
+    BYTE* Thumbnail;
+}HEADER_ADDTIONAL;
 
-}HEADER;
 
 typedef struct _DQT_markers{
     WORD DQT_mark;
+    BYTE* DQT_tables;
     WORD data_len;
-}DQT;
+}DQT_MARK;
+
+typedef struct _SOF_markers {
+    WORD SOF_len;
+    BYTE* SOF_data;
+}SOF_MARK;
+
+typedef struct _HUFF_tables {
+    BYTE huff_id;
+    WORD huff_len;
+    BYTE* huff_table;
+}HUF_TABLE;
+
+typedef struct _DHT_markers {
+    int numOfhuff;
+    HUF_TABLE huf_tables[4];
+}DHT_MARK;
+
+typedef struct _SOS_markers {
+    WORD SOS_len;
+    BYTE Comp_count;
+    BYTE Comp_id_Y;
+    BYTE huff_id;
+    BYTE Comp_id_Cb;
+    BYTE huff_id_Cb;
+    BYTE Comp_id_Cr;
+    BYTE huff_id_Cr;
+
+    //dct//
+    BYTE spec_start;
+    BYTE spec_end;
+    BYTE succ_approx;
+
+    BYTE* SOS_data;
+}SOS_MARK;
+
+typedef struct _DRI_marker {
+    WORD DRI_mark;
+    WORD scan_len;
+    WORD restart_interval;
+}DRI_MARK;
 
 #define BYTE unsigned char 
 #define WORD unsigned short
@@ -39,11 +88,11 @@ typedef struct _DQT_markers{
 #define SOI 0xD8FF // Start of Image
 #define EOI 0XD9 // End of Image
 #define APP0 0xE0FF //application specific data
-#define SOF 0xC0 //Start of Frame
+#define SOF 0xC0FF //Start of Frame
 #define DQT 0xDBFF //Define Quantization Tables
-#define DHT 0xC4 //Deine Huffman Tables
-#define SOS 0xDA //Start of Scan
-#define DRI 0xDD //Define Restart interval
+#define DHT 0xC4FF //Deine Huffman Tables
+#define SOS 0xDAFF //Start of Scan
+#define DRI 0xDDFF //Define Restart interval
 #define COM 0xFE //Commnet
 
 
@@ -58,5 +107,10 @@ WORD to_big(WORD a) {
         ((WORD)bytes[1] << 0);
     return result;
 }
+
+
+
+int readHeader(FILE*, HEADER*,HEADER_ADDTIONAL*, WORD);
+void readDQT(FILE* jpg, DQT_MARK* dqt_mark, WORD read);
 
 #endif
