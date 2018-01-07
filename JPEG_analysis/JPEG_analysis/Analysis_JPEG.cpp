fbe329ee393ea080705884da7dd049f9f5b3ebb2
rina2 <lhwlina1130@naver.com>
2018-01-07 18:13:40 +0900

    complete before SOF

--- JPEG_analysis/JPEG_analysis/Analysis_JPEG.cpp
+++ JPEG_analysis/JPEG_analysis/Analysis_JPEG.cpp
@@ -10,14 +10,18 @@
 
 int main() {
 
+    char tmp[5];
     FILE* jpg;
     fopen_s(&jpg, "test.jpg", "rb");
     if (jpg == NULL) {
         printf("error");
         return -1;
     }
+
+    
     HEADER header;
-    fread(&header, 20, 1, jpg);
+    
+    fread(&header, sizeof(HEADER), 1, jpg);
     if (header.soi != SOI) {
         printf("different SOI");
         return -1;
@@ -26,10 +30,34 @@ int main() {
         printf("different APP0");
         return -1;
     }
-    printf("soi : %x\n", header.soi);
-    //additional data length
-    printf("additional data length: %d\n", header.add_len);
 
+    header.data_len = to_big(header.data_len);
+    
+    BYTE* additional_data = (BYTE*)malloc(sizeof(BYTE)*(header.data_len - 2));
+    fread(additional_data, sizeof(BYTE)*(header.data_len-2), 1, jpg);
+    
+    //To Do: check additional data  --> Read Commnet, too ( FFEE )_
 
+   
 
+    ////////DQT : Define Quantization Tables/////////////
+    WORD isDQT;
+    BYTE* DQT_table[4];
+    
+
+    while (fread(&isDQT, sizeof(WORD), 1, jpg)) {
+        if (isDQT != DQT) break;
+        WORD table_size;
+        BYTE idx;
+        fread(&table_size, sizeof(WORD), 1, jpg);
+        fread(&idx, sizeof(BYTE), 1, jpg);
+        table_size = to_big(table_size);
+        DQT_table[idx] = (BYTE*)malloc(sizeof(BYTE)*(table_size - 3));
+        fread(DQT_table[idx], sizeof(BYTE)*(table_size - 3), 1, jpg);
+        idx++;
+    };
+
+   fseek(jpg, -2L, SEEK_CUR);
+    
 }
+
--- JPEG_analysis/JPEG_analysis/jpegdec.h
+++ JPEG_analysis/JPEG_analysis/jpegdec.h
@@ -1,24 +1,32 @@
 #ifndef _JPEGDEC_H__
 #define _JPEGDEC_H__
 
+#pragma pack(1)
 typedef struct {
     WORD soi;
     WORD app;
-    WORD add_len;
-    DWORD jfif;
+    WORD data_len;
+    /*
+    DWORD identifier;
     BYTE null;
-    WORD version;
-    BYTE numOfPix;
-    WORD hori_resol;
-    WORD perp_resol;
-    WORD show;
-    
+    BYTE major_v;
+    BYTE minor_v;
+    BYTE units;
+    WORD X_density;
+    WORD Y_density;
+    BYTE Xthumbnail;
+    BYTE Ythumbnail;
+    */
 
 }HEADER;
 
+typedef struct _DQT_markers{
+    WORD DQT_mark;
+    WORD data_len;
+}DQT;
 
 #define BYTE unsigned char 
-#define WORD unsigned short int
+#define WORD unsigned short
 
 #define DWORD unsigned int
 #define SDWORD signed int
@@ -32,10 +40,23 @@ typedef struct {
 #define EOI 0XD9 // End of Image
 #define APP0 0xE0FF //application specific data
 #define SOF 0xC0 //Start of Frame
-#define DQT 0xDB //Define Quantization Tables
+#define DQT 0xDBFF //Define Quantization Tables
 #define DHT 0xC4 //Deine Huffman Tables
 #define SOS 0xDA //Start of Scan
 #define DRI 0xDD //Define Restart interval
 #define COM 0xFE //Commnet
+
+
+
+WORD to_big(WORD a) {
+    unsigned char bytes[2];
+    bytes[0] = (unsigned char)((a >> 0) & 0xff);
+    bytes[1] = (unsigned char)((a >> 8) & 0xff);
+
+    WORD result;
+    result = ((WORD)bytes[0] << 8) |
+        ((WORD)bytes[1] << 0);
+    return result;
+}
 
 #endif
